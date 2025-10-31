48581867dbcbdd8f33f8b3bd9bdd310067a312bf
Asti22 <uun3461@gmail.com>
2025-10-31 15:19:46 +0700

    membuat fungsi FormulisPendafataran

---
+++ app/src/main/java/com/example/inputpenggunatugas/ui/theme/teksEdit.kt
@@ -0,0 +1,7 @@
+package com.example.inputpenggunatugas.ui.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormulirPendaftaran(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
