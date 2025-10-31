6810c2a9644ffe546a3ac9230b7478cecfa0bc57
Asti22 <uun3461@gmail.com>
2025-10-31 15:21:22 +0700

    membuat variable untuk bagian inputan penggun nantinya

--- app/src/main/java/com/example/inputpenggunatugas/ui/theme/teksEdit.kt
+++ app/src/main/java/com/example/inputpenggunatugas/ui/theme/teksEdit.kt
@@ -5,3 +5,8 @@ import androidx.compose.runtime.Composable
 @Composable
 fun FormulirPendaftaran(modifier: Modifier = Modifier) {
     val context = LocalContext.current
+
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
