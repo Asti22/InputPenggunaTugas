2888c98331c25484f3f5de56c23ca3f120acdc32
Asti22 <uun3461@gmail.com>
2025-10-31 16:00:56 +0700

    Menambahkan tombol submit

--- app/src/main/java/com/example/inputpenggunatugas/ui/theme/teksEdit.kt
+++ app/src/main/java/com/example/inputpenggunatugas/ui/theme/teksEdit.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.OutlinedTextField
@@ -119,6 +122,16 @@ fun FormulirPendaftaran(modifier: Modifier = Modifier) { // tambahkan default Mo
                         singleLine = true,
                         modifier = Modifier.fillMaxWidth()
                     )
+                    Button (
+                        onClick = { /* aksi submit nanti */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(dimensionResource(R.dimen.button_height)),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF7E57C2)),
+                        shape = RoundedCornerShape(12.dp)
+                    ){
+
+                    }
 
                 }
 
