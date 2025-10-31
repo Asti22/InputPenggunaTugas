619ede86c7668d79c20692b7b3a792f5fe803523
Asti22 <uun3461@gmail.com>
2025-10-31 15:27:50 +0700

    membuat variable untuk bagian inputan genderlist

--- app/src/main/java/com/example/inputpenggunatugas/ui/theme/teksEdit.kt
+++ app/src/main/java/com/example/inputpenggunatugas/ui/theme/teksEdit.kt
@@ -1,12 +1,22 @@
 package com.example.inputpenggunatugas.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 
 @Composable
-fun FormulirPendaftaran(modifier: Modifier = Modifier) {
+fun FormulirPendaftaran(modifier: Modifier) {
     val context = LocalContext.current
 
-    var nama by remember { mutableStateOf("") }
-    var alamat by remember { mutableStateOf("") }
-    var jenisKelamin by remember { mutableStateOf("") }
-    var status by remember { mutableStateOf("") }
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenisKelamin by remember { mutableStateOf(value = "") }
+    var status by remember { mutableStateOf(value = "") }
+
+    var genderList = listOf(
+        stringResource(id = R.string.gender_male),
+        stringResource(id= R.string.gender_female)
+    )
